3c1e70c0c823512f9ae63ed586239d7978d11270
ansakoy <ansakoy@gmail.com>
2022-06-01 18:04:32 +0300

    Add first migrations

--- djmigr/library/models.py
+++ djmigr/library/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """Author
+    """
+    full_name = models.CharField(
+        "Author's name",
+        max_length=500,
+    )
+    birth_date = models.DateField(
+        "Date of birth",
+        null=True,
+        default=None,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.full_name
+
+
+class Book(models.Model):
+    """Book
+    """
+    title = models.CharField(
+        "Title",
+        max_length=1000,
+    )
+    author = models.ForeignKey(
+        Author,
+        related_name="books",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
+
+    def __str__(self):
+        return self.title
